c79a44ef2f76401b1f0de501cbba2b8f021711b1
nataliamoiseenko <natallia.moiseenko@gmail.com>
2023-05-08 20:46:30 +0200

    feat: task 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,15 @@
+const fs = require('fs');
+const path = require('path');
 
+const sourcePath = path.join(__dirname, 'files');
+const targetPath = path.join(__dirname, 'files-copy');
+
+function copyDir() {
+  fs.cp(sourcePath, targetPath, { recursive: true }, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+};
+
+copyDir();
